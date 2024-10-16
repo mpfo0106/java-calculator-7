c8262e60b3fd051b331586bfe3edf8230f15b9e5
mpfo0106 <mpfo0106@gmail.com>
2024-10-16 21:01:35 +0900

    feat: 사용자로부터 문자열 입력 받기

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+    public void run(){
+        try {
+            String input = getInput();
+        }
+        catch (IllegalArgumentException e){
+
+        }
+    }
+    private String getInput(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        if(input == null || input.trim().isEmpty()){
+            throw new IllegalArgumentException("입력값이 비어있습니다.");
+        }
+        return input;
+    }
+}
